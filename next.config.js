091fba871644349b8337f60bc44df9d358fae66f
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-03-20 06:19:17 +0100

    Deploiement en dev

--- next.config.js
+++ next.config.js
@@ -6,7 +6,15 @@ const nextConfig = {
   env: {
     ACCES_TOKEN: process.env.ACCES_TOKEN,
     API_URL: process.env.API_URL,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/items/:path',
+        destination: `${process.env.API_URL}/items/:path`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
